c6e4ec713cb03ecf96aaf341b899f3a448d700db
ElonaKajtazi <elona.kajtazi@gmail.com>
2022-07-12 20:06:39 +0200

    created state, and rendered main page with js!!

--- src/main.ts
+++ src/main.ts
@@ -1,47 +1,167 @@
 import "./style.css";
 
-let state = {};
+type Store = {
+  id: number;
+  type: string;
+  name: string;
+  image: string;
+  price: number;
+  discountedPrice?: number;
+  dateEntered: string;
+  stock: number;
+};
+type State = {
+  store: Store[];
+};
+
+let state: State = {
+  store: [
+    {
+      id: 1,
+      type: "Guys",
+      name: "Crewneck T-Shirt 3-Pack",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1",
+      price: 40,
+      discountedPrice: 21.99,
+      dateEntered: "2021/08/10",
+      stock: 10,
+    },
+    {
+      id: 2,
+      type: "Girls",
+      name: "Smocked Tiered Mini Dress",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_359-1220-1911-805_prod1",
+      price: 29,
+      dateEntered: "2021/07/10",
+      stock: 5,
+    },
+    {
+      id: 3,
+      type: "Girls",
+      name: "Gilly Hicks Cozy Joggers",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_346-1252-0485-116_prod1",
+      price: 27,
+      dateEntered: "2021/05/06",
+      stock: 15,
+    },
+    {
+      id: 4,
+      type: "Guys",
+      name: "Gilly Hicks Go Energize 2-in-1 Running Short",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_346-1614-0521-900_prod1",
+      price: 29,
+      dateEntered: "2021/07/10",
+      stock: 20,
+    },
+    {
+      id: 5,
+      type: "Guys",
+      name: "Gilly Hicks Woven Boxer",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_309-1408-0413-136_prod1",
+      price: 12,
+      dateEntered: "2021/07/10",
+      stock: 2,
+    },
+    {
+      id: 6,
+      type: "Guys",
+      name: "Gilly Hicks Smiley Socks 2-Pack",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_354-1020-0010-900_prod1",
+      price: 14,
+      dateEntered: "2021/06/11",
+      stock: 1,
+    },
+    {
+      id: 7,
+      type: "Guys",
+      name: "Stretch Poplin Slim Fit shirt",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_325-1072-0609-214_prod1",
+      price: 29,
+      discountedPrice: 20.3,
+      dateEntered: "2020/02/10",
+      stock: 13,
+    },
+    {
+      id: 8,
+      type: "Guys",
+      name: "Must-Have Crewneck T-Shirt 5-Pack",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_324-1072-0132-108_prod1",
+      price: 55,
+      discountedPrice: 34.99,
+      dateEntered: "2021/08/10",
+      stock: 12,
+    },
+    {
+      id: 9,
+      type: "Guys",
+      name: "Print Logo Graphic Tee",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_323-1957-2978-908_prod1",
+      price: 25,
+      discountedPrice: 11.99,
+      dateEntered: "2021/04/10",
+      stock: 17,
+    },
+    {
+      id: 10,
+      type: "Girls",
+      name: "Ultra High-Rise Dad Sweatpants",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_347-1074-0669-100_prod1",
+      price: 35,
+      discountedPrice: 14.99,
+      dateEntered: "2021/08/01",
+      stock: 6,
+    },
+    {
+      id: 11,
+      type: "Girls",
+      name: "Crop Oversized Hoodie",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_352-3541-0604-100_prod1",
+      price: 35,
+      discountedPrice: 12.99,
+      dateEntered: "2021/08/01",
+      stock: 8,
+    },
+    {
+      id: 12,
+      type: "Girls",
+      name: "Gilly Hicks Socks 2-Pack",
+      image:
+        "https://img.hollisterco.com/is/image/anf/KIC_354-1013-0002-100_prod1",
+      price: 14,
+      discountedPrice: 5.99,
+      dateEntered: "2021/07/10",
+      stock: 10,
+    },
+  ],
+};
 
 function render() {
-  //     <header class="header">
-  //     <div class="header__left">
-  //       <h1 class="logo">Hollixton</h1>
-  //       <nav>
-  //         <ul class="left__nav-list">
-  //           <li class="nav-list__item">Girls</li>
-  //           <li class="nav-list__item">Guys</li>
-  //           <li class="nav-list__item">Sale</li>
-  //         </ul>
-  //       </nav>
-  //     </div>
-  //     <div class="header__right">
-  //       <nav>
-  //         <ul class="right__nav-list">
-  //           <li class="nav-list__item">
-  //             <span class="material-symbols-outlined"> search </span>
-  //           </li>
-  //           <li class="nav-list__item">
-  //             <span class="material-symbols-outlined"> person </span>
-  //           </li>
-  //           <li class="nav-list__item">
-  //             <span class="material-symbols-outlined"> shopping_bag </span>
-  //           </li>
-  //         </ul>
-  //       </nav>
-  //     </div>
-  //   </header>
+  // finding the container, and clearing it
   let appEl = document.querySelector("#app");
   if (appEl === null) return;
   appEl.textContent = "";
+
+  // Header
   let headerEl = document.createElement("header");
   headerEl.className = "header";
-
+  //Header left
   let leftDivEl = document.createElement("div");
   leftDivEl.className = "header__left";
 
-  let titleEl = document.createElement("h1");
-  titleEl.className = "logo";
-  titleEl.textContent = "Hollixton";
+  let logoEl = document.createElement("h1");
+  logoEl.className = "logo";
+  logoEl.textContent = "Hollixton";
 
   let leftNavE = document.createElement("nav");
 
@@ -60,28 +180,157 @@ function render() {
   leftLiItem2.className = "nav-list__item";
   leftLiItem2.textContent = "Sale";
 
-  leftDivEl.append(titleEl, leftNavE);
+  leftDivEl.append(logoEl, leftNavE);
   leftNavE.append(leftList);
   leftList.append(leftLiItem, leftLiItem1, leftLiItem2);
-
+  //Header right
   let rightDivEl = document.createElement("div");
   rightDivEl.className = "header__right";
+
   let rightNavE = document.createElement("nav");
+
   let rightList = document.createElement("ul");
   rightList.className = "right__nav-list";
+
   let rightLiItem = document.createElement("li");
   rightLiItem.className = "nav-list__item";
   rightLiItem.textContent = "🔍";
+
   let rightLiItem1 = document.createElement("li");
   rightLiItem1.className = "nav-list__item";
   rightLiItem1.textContent = "👤";
+
   let rightLiItem2 = document.createElement("li");
   rightLiItem2.className = "nav-list__item";
   rightLiItem2.textContent = "🛒";
+
   rightNavE.append(rightList);
   rightList.append(rightLiItem, rightLiItem1, rightLiItem2);
   rightDivEl.append(rightNavE);
   headerEl.append(leftDivEl, rightDivEl);
   appEl.append(headerEl);
+
+  //   <main class="main">
+  //   <h2 class="title">Home</h2>
+  //   <ul class="shoping-items">
+  //     <li class="shopping-item">
+  //       <img
+  //         class="shoping-item__img"
+  //         src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1"
+  //         alt="T-shirt"
+  //       />
+  //       <h3 class="item-name">Crewneck T-shirt 3-Pack</h3>
+  //       <p class="item-price">
+  //       <span class="previous-item-price">£40</span>
+  //       <span class="discounted-item-price">£21.99</span>
+  //       </p>
+  //     </li>
+  //     <li class="shopping-item">
+  //       <img
+  //         class="shoping-item__img"
+  //         src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1"
+  //         alt="T-shirt"
+  //       />
+  //       <h3 class="item-name">Crewneck T-shirt 3-Pack</h3>
+  //       <p class="item-price">£19.99</p>
+  //     </li>
+  //     <li class="shopping-item">
+  //       <img
+  //         class="shoping-item__img"
+  //         src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1"
+  //         alt="T-shirt"
+  //       />
+  //       <h3 class="item-name">Crewneck T-shirt 3-Pack</h3>
+  //       <p class="item-price">$19.99</p>
+  //     </li>
+  //     <li class="shopping-item">
+  //       <img
+  //         class="shoping-item__img"
+  //         src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1"
+  //         alt="T-shirt"
+  //       />
+  //       <h3 class="item-name">Crewneck T-shirt 3-Pack</h3>
+  //       <p class="item-price">$19.99</p>
+  //     </li>
+  //     <li class="shopping-item">
+  //       <img
+  //         class="shoping-item__img"
+  //         src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1"
+  //         alt="T-shirt"
+  //       />
+  //       <h3 class="item-name">Crewneck T-shirt 3-Pack</h3>
+  //       <p class="item-price">$19.99</p>
+  //     </li>
+  //     <li class="shopping-item">
+  //       <img
+  //         class="shoping-item__img"
+  //         src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1"
+  //         alt="T-shirt"
+  //       />
+  //       <h3 class="item-name">Crewneck T-shirt 3-Pack</h3>
+  //       <p class="item-price">$19.99</p>
+  //     </li>
+  //     <li class="shopping-item">
+  //       <img
+  //         class="shoping-item__img"
+  //         src="https://img.hollisterco.com/is/image/anf/KIC_324-1085-0123-100_prod1"
+  //         alt="T-shirt"
+  //       />
+  //       <h3 class="item-name">Crewneck T-shirt 3-Pack</h3>
+  //       <p class="item-price">$19.99</p>
+  //     </li>
+  //   </ul>
+  // </main>
+  let mainEl = document.createElement("main");
+  mainEl.className = "main";
+
+  let titleEl = document.createElement("h2");
+  titleEl.className = "title";
+  titleEl.textContent = "Home";
+
+  let shopingItemsList = document.createElement("ul");
+  shopingItemsList.className = "shoping-items";
+
+  for (let item of state.store) {
+    let shopingItemEl = document.createElement("li");
+    shopingItemEl.className = "shopping-item";
+
+    let shopingItemImageEl = document.createElement("img");
+    shopingItemImageEl.className = "shoping-item__img";
+    shopingItemImageEl.src = item.image;
+    shopingItemImageEl.alt = item.name;
+
+    let shopingItemNameEl = document.createElement("h3");
+    shopingItemNameEl.className = "item-name";
+    shopingItemNameEl.textContent = item.name;
+
+    let shopingItemPriceEl = document.createElement("p");
+    shopingItemPriceEl.className = "item-price";
+    let shopingItemPriceSpanEl = document.createElement("span");
+    shopingItemPriceSpanEl.className = "previous-item-price";
+    shopingItemPriceSpanEl.textContent = "£" + item.discountedPrice;
+    let shopingItemPriceSpanEl1 = document.createElement("span");
+    shopingItemPriceSpanEl1.className = "discounted-item-price";
+    shopingItemPriceSpanEl1.textContent = "£" + item.price;
+
+    if (item.discountedPrice) {
+      shopingItemPriceEl.append(
+        shopingItemPriceSpanEl,
+        shopingItemPriceSpanEl1
+      );
+    } else {
+      shopingItemPriceEl.append(shopingItemPriceSpanEl1);
+    }
+
+    shopingItemEl.append(
+      shopingItemImageEl,
+      shopingItemNameEl,
+      shopingItemPriceEl
+    );
+    shopingItemsList.append(shopingItemEl);
+  }
+  mainEl.append(titleEl, shopingItemsList);
+  appEl.append(mainEl);
 }
 render();
+window.state = state;
